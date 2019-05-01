46492bddbe066918c16ce2dc44e524102fca7182
root <root@cloud.dgkim.net>
2019-05-01 18:13:42 +0900

    .

--- application/config/config.php
+++ application/config/config.php
@@ -22,8 +22,7 @@
 | a PHP script and you can easily do that on your own.
 |
 */
-//$config['base_url'] = '';
-$config['base_url'] = 'http://survey.handong.dev.readershp.com/survey/';
+$config['base_url'] = '';
 
 /*
 |--------------------------------------------------------------------------
